0b1a63aa15bd1af0c59e2300a07424b96ab00fa6
Kyle Raney <raney24fcb@gmail.com>
2017-02-23 20:55:15 -0500

    portfolio detail page

--- assets/js/carousel-flex.js
+++ assets/js/carousel-flex.js
@@ -1,11 +1,31 @@
 
 $(document).ready(function () {
-	
+	var companies = {
+		mobile_serve: [
+			"Mobile Serve",
+			4
+		],
+		pc_usa: [
+			"PC USA",
+			3
+		]
+	};
 	$('.box-small').on('click',function(){
-	    var jsonData = $.ajax("../assets/js/portfolio.json", function() {
-	    	console.log("Success");
-	    });
-	    console.log("Here" + jsonData);
+
+		var company = ($(this).attr("id"));
+		var shortname = companies[company][0];
+		var images = companies[company][1];
+
+		$('#company-header').hide().text(shortname).fadeIn(900);
+
+		$( '.item' ).each(function(index, element) {
+			var newSrc = '/assets/img/carousel/' + company + index + '.jpg';
+			$(element).find('img').fadeOut(200, function() {
+				$(element).find('img').attr('src', newSrc);
+			}).fadeIn(500);
+			// $(element).find("img").attr('src', newSrc);
+		});
+
 	});
 
 });
